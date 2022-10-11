19b046d5c9e29729cf523696e6d4ae1046d54602
sehee <kimsehee7414@naver.com>
2022-10-11 22:31:44 +0900

    New thing

---
+++ app/src/main/java/edu/handong/ict/dep/prayerbankjava/Mypage.java
@@ -0,0 +1,14 @@
+package edu.handong.ict.dep.prayerbankjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mypage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mypage);
+    }
+}
